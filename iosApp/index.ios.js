57cb5f965cfb2d0cbb1eacf66a61488730371d15
danielrobertson <danielrobertson733@gmail.com>
2017-04-29 17:53:26 -0500

    fix broken build

--- iosApp/index.ios.js
+++ iosApp/index.ios.js
@@ -5,28 +5,10 @@ import {
   Text,
   View
 } from 'react-native';
-import keys from './IDIHackathon-a33b9dcfefb4.json';
-const Vision = require('@google-cloud/vision');
-const projectId = keys.project_id;
-const visionClient = Vision({
-  projectId: projectId
-});
 const fileName = 'https://images.gr-assets.com/quotes/1387506524p8/2534.jpg';
 import  Camera from 'react-native-camera';
 
-
 export default class iosApp extends Component {
-  /*componentDidMount(){
-    visionClient.detectFulltext(fileName)
-      .then((results) => {
-        const labels = results[0];
-        console.log('Labels: ');
-        labels.forEach((label) => console.log(label));
-      })
-      .catch((err) => {
-        console.log('ERROR: ', err);
-      });
-  }*/
   render() {
     return (
       <View style={styles.container}>
