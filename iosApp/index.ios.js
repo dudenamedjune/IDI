65dbc85b2e8ebcf0fef89019405aafe1178342f7
Ryan Ngoh <rxn120230@utdallas.edu>
2017-04-29 17:10:18 -0500

    added gcloud

--- iosApp/index.ios.js
+++ iosApp/index.ios.js
@@ -3,7 +3,6 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -11,8 +10,26 @@ import {
   Text,
   View
 } from 'react-native';
+import keys from './IDIHackathon-a33b9dcfefb4.json';
+const Vision = require('@google-cloud/vision');
+const projectId = keys.project_id;
+const visionClient = Vision({
+  projectId: projectId
+});
+const fileName = 'https://images.gr-assets.com/quotes/1387506524p8/2534.jpg';
 
 export default class iosApp extends Component {
+  componentDidMount(){
+    visionClient.detectFulltext(fileName)
+      .then((results) => {
+        const labels = results[0];
+        console.log('Labels: ');
+        labels.forEach((label) => console.log(label));
+      })
+      .catch((err) => {
+        console.log('ERROR: ', err);
+      });
+  }
   render() {
     return (
       <View style={styles.container}>
