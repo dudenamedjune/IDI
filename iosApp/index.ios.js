b11352124e8a2ad67a1ded8a5ac59c31284d8933
june.k.lara@gmail.com <june.k.lara@gmail.com>
2017-04-29 16:47:54 -0500

    camera working

--- iosApp/index.ios.js
+++ iosApp/index.ios.js
@@ -11,43 +11,55 @@ import {
   Text,
   View
 } from 'react-native';
+import  Camera from 'react-native-camera';
+
+
+
 
 export default class iosApp extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Camera
+            ref={(cam) => {
+              this.camera = cam;
+            }}
+            style={styles.preview}
+            aspect={Camera.constants.Aspect.fill}>
+            <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+          </Camera>
+
       </View>
     );
   }
+  takePicture() {
+   const options = {};
+   //options.location = ...
+   this.camera.capture({metadata: options})
+     .then((data) => console.log(data))
+     .catch(err => console.error(err));
+ }
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: 'row',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
 });
 
 AppRegistry.registerComponent('iosApp', () => iosApp);
